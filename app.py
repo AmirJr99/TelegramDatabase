a61d54c92f1ba7e0796a81501eed82c3b62752e6
Hamed Mohammadvand <70788776+h4medgit@users.noreply.github.com>
2021-12-19 12:08:31 +0330

    Add files via upload

---
+++ app.py
@@ -0,0 +1,20 @@
+import json
+
+ff = open('dataset.txt', 'a+', encoding='utf-8')
+with open('rahmatism-xaj.txt', 'r', encoding='utf-8') as f:
+    for x, i in enumerate(f, start=1):
+        if x % 2 == 0:
+            ff.write(i)
+
+
+
+def makingfunc():
+    ff = open('dataset.txt', encoding='utf-8')
+    data = [x.replace('\n', '') for x in ff.readlines()]
+    global database
+    database = []
+    for x in data:
+        data1 = json.loads(eval(x)['message'])
+        database.append([data1['first_name'], data1['last_name'], data1['phone'], data1['id']])
+makingfunc()
+
